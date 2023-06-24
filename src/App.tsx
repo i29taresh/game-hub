a15ee90196d8f353a595d70351b1c9e8e783a8eb
i29taresh <i.amtaresh29@gmail.com>
2023-06-24 13:10:01 +0530

    Highlight the selected Genre

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area={"aside"} paddingX={5}>
-          <GenreList onSelectGerne={(genre) => setSelectedGenre(genre)} />
+          <GenreList selectedGenre={selectedGenre} onSelectGerne={(genre) => setSelectedGenre(genre)} />
         </GridItem>
       </Show>
       <GridItem area={"main"}>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,12 @@
-import { Button, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import { Button, HStack, Image, List, ListItem, Spinner} from "@chakra-ui/react";
 import useGenre, { Genre } from "../hooks/useGenre";
 
 interface Props {
   onSelectGerne: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({onSelectGerne}: Props) => {
+const GenreList = ({selectedGenre, onSelectGerne}: Props) => {
   const { data, isLoading, error } = useGenre();
   if(error) return null;
   if (isLoading) return <Spinner />;
@@ -19,7 +20,7 @@ const GenreList = ({onSelectGerne}: Props) => {
               boxSize={"32px"}
               borderRadius={8}
             />
-            <Button fontSize="lg" variant={'link'} onClick={() => onSelectGerne(genre)}>{genre.name} </Button>
+            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal' } fontSize="lg" variant={'link'} onClick={() => onSelectGerne(genre)}>{genre.name} </Button>
           </HStack>
         </ListItem>
       ))}
